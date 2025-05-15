1d09839f9f28afde25c1355122e54cd63ee763df
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-05-15 17:02:13 +0200

    🔧 Mise à jour de l'issuer Keycloak dans la configuration de l'environnement de production : changement de l'URL pour refléter le nouveau backend. Cela assure une connexion correcte pour l'authentification.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   keycloak: {
     // Si votre backend Render gère aussi Keycloak ou si vous avez une API pour l'authentification
-    issuer: 'https://todoappbackend-h61b.onrender.com/realms/ToDoApp', // à ajuster selon votre configuration
+    issuer: 'https://todoappbackend-13go.onrender.com/realms/ToDoApp', // à ajuster selon votre configuration
     clientId: 'todo-app-client',
     redirectUri: window.location.origin,
   },
