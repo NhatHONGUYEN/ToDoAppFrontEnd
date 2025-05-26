cbbc7856d8b63f6524509354f36bd454eb4a773e
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-05-26 15:04:56 +0200

    🔧 Mise à jour de la configuration de l'environnement de production : ajout de remplacements de fichiers pour l'environnement et mise à jour de l'URL de l'API ainsi que de l'issuer Keycloak. Cela assure une intégration correcte avec le backend et les services d'authentification.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://todoappbackend-production-3438.up.railway.app/api/tasks',
   keycloak: {
-    issuer: 'https://todoappnhat.netlify.app/realms/ToDoApp',
+    issuer:
+      'https://todoappkeycloak-production.up.railway.app/realms/todo-realm',
     clientId: 'todo-frontend-client',
     redirectUri: window.location.origin,
   },
