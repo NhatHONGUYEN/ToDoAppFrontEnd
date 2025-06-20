9b267f874da300012043689eabed1e60b1ccd7b1
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-06-20 15:46:10 +0200

    🔧 Mise à jour de l'URL de l'issuer Keycloak dans le fichier d'environnement, passant de http://localhost:81 à http://localhost:8180 pour le développement.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/tasks',
   keycloak: {
-    issuer: 'http://localhost:81/realms/todo-backend-local',
+    issuer: 'http://localhost:8180/realms/todo-backend-local',
     clientId: 'todo-backend-local',
     // Changez cette ligne pour spécifier l'URL complète au lieu de window.location.origin
     redirectUri: 'http://localhost:4200',
